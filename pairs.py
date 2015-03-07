04c554ea5798dc80cfa120eefa8db1c305d1a6bd
Benjamin Kuykendall <brk2117@columbia.edu>
2015-03-06 20:31:02 -0500

    final version

--- pairs.py
+++ pairs.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-  
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
 
 from itertools import *
+import random
 import copy
 import os
 
-choices = ['Adam', 'Ben', 'Noah', 'Merhzad', 'Steven', 'Theo']
+choices = ['Adam', 'Ben', 'Noah', 'Mehrzad', 'Steven', 'Theo']
+#choices = ['A','B','C','D']
 
 def main():	
   all_ranks = dict()
@@ -20,11 +38,13 @@ def main():
       sum_squares += pow((all_ranks[p[0]][p[1]]),2) + pow((all_ranks[p[1]][p[0]]),2)
     choice_sum.append( (sum_squares, choice) )
   min_val = 10000
-  min_choice = False
+  min_choice = []
   for cs in choice_sum:
-    if cs[0] < min_val:
-      min_choice = cs[1]
-  print min_choice
+    if cs[0] == min_val:
+      min_choice.append(cs[1])
+    elif cs[0] < min_val:
+      min_choice = [cs[1]]
+  print random.choice(min_choice)
     
 def get_choice_dictionary(exclude):
   ranks = dict()
